8913d3e411c772f1a3fe6adf2c3ca6ab2af7e3cb
Jacob Nickell <nickell.35@osu.edu>
2017-10-20 14:17:16 -0400

    implemented methods

--- chatRoom/chatroom/src/main/java/com/techelevator/chatroom/BlogPostDAO.java
+++ chatRoom/chatroom/src/main/java/com/techelevator/chatroom/BlogPostDAO.java
@@ -4,9 +4,9 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface BlogPostDAO {
-	public void createNewBlogPost();
+	public BlogPost createNewBlogPost();
 	public List<BlogPost> allBlogPosts();
 	public List<BlogPost> blogPostsBetweenDates(LocalDate fromDate, LocalDate toDate);
 	public List<BlogPost> blogPostSinceDAte(LocalDate sinceDate);
-	public void deleteBlogPost();
+	public boolean deleteBlogPost();
 }
--- chatRoom/chatroom/src/main/java/com/techelevator/chatroom/JDBCBlogPostDAO.java
+++ chatRoom/chatroom/src/main/java/com/techelevator/chatroom/JDBCBlogPostDAO.java
@@ -1,13 +1,36 @@
 package com.techelevator.chatroom;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
-public class JDBCBlogPostDAO implements BlogPostDAO {
+import javax.sql.DataSource;
 
-	public List<BlogPost> allBlogPosts() {
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+public class JDBCBlogPostDAO implements BlogPostDAO {
+	private JdbcTemplate jdbcTemplate;
+	
+	public JDBCBlogPostDAO(DataSource dataSource) {
+		jdbcTemplate = new  JdbcTemplate(dataSource);
+	}
+
+	public BlogPost createNewBlogPost() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public List<BlogPost> allBlogPosts() {
+		List<BlogPost> blogList = new ArrayList<BlogPost>();
+		String sqlQuery = "SELECT * " +
+						  "FROM blog_post";
+		SqlRowSet rowSet = jdbcTemplate.queryForRowSet(sqlQuery);
+		while (rowSet.next()) {
+			BlogPost post = makeBlogPostFromRowSet(rowSet);
+			blogList.add(post);
+		}
+		return blogList;
 	}
 
 	public List<BlogPost> blogPostsBetweenDates(LocalDate fromDate, LocalDate toDate) {
@@ -19,5 +42,22 @@ public class JDBCBlogPostDAO implements BlogPostDAO {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	public boolean deleteBlogPost() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private BlogPost makeBlogPostFromRowSet(SqlRowSet results) {
+		BlogPost post = new BlogPost();
+		post.setAccountId(results.getInt("account_id"));
+		post.setPostId(results.getInt("id"));
+		post.setPostTitle(results.getString("title"));
+		post.setPostBody(results.getString("body"));
+		post.setDateTime(results.getDate("date_time").toLocalDate());
+		post.setNotes(results.getString("notes"));
+		return post;
+	}
+	
 
 }
